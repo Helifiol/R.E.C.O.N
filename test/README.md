678323d7a1b14e3bd467b56808f6158939589924
Heli fiol <66737232+Helifiol@users.noreply.github.com>
2024-03-02 19:47:18 -0800

    Delete test directory

--- test/README.md
+++
@@ -1,2 +0,0 @@
-# keylogger
-Python keylogger program with pynput, a webserver to send and recieve keylogger commands and a nuke program to leave no trace once you are done
--- test/tester.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# Example 1: Running a simple shell command
-result = subprocess.run(['ls', '-l'], capture_output=True, text=True)
-# print("Output:", type(result.stdout.encode('utf-8')))
-
-s = "ls -la"
-print(s.split())
